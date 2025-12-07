4a3c92e8ba41dc25fa2425740845ff563a6c87ff
mikafederlin <mikafederlin@gmail.com>
2025-12-07 12:39:08 +0000

    Lösung gesichert

--- 07_Dezember/main.py
+++ 07_Dezember/main.py
@@ -1,6 +1,7 @@
 # 🕯️ Tag 7: Brav oder Frech?
 
 artigkeit = 8  # Ändere diese Zahl später zum Testen (z.B. auf 2)
+artigkeit=input("wie artig war ich von 0-10?:")
 
 # Schreibe hier die if-Abfrage:
 # Prüfe: Ist artigkeit größer als (>) 5?
@@ -9,5 +10,10 @@ artigkeit = 8  # Ändere diese Zahl später zum Testen (z.B. auf 2)
 
 # else:
 
-    # Sonst: Gib "Kohle..." aus
+    # Sonst: Gib "Kohle...aus.
+
+if int(artigkeit) > 5:
+    print("Geschenk")
+else:
+    print("kohle")
 
