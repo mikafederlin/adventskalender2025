022375c988590f708e5027f8a47d08a2e785c0b6
mikafederlin <mikafederlin@gmail.com>
2025-12-12 20:10:53 +0000

    Lösung gesichert

--- 11_Dezember/main.py
+++ 11_Dezember/main.py
@@ -1,13 +1,13 @@
 # 🕯️ Tag 11: Der Adressaufkleber
 
-name = "Lena"
-stadt = "Berlin"
+name = "Mika"
+stadt = "Herrenshoff"
 
 # Aufgabe:
 # Baue den Satz "Das Paket für ... geht nach ..." mit einem f-String.
 # 1. Schreibe ein f vor den String: f"..."
 # 2. Setze die Variablen name und stadt in geschweifte Klammern {} an die richtige Stelle.
 
-satz = ...
+satz =f"Das Paket für {name} geht nach {stadt}"
 
 print(satz)
