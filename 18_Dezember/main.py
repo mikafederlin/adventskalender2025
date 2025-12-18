62b4cd3383f3e84118a4af56abf60410e3af333d
mikafederlin <mikafederlin@gmail.com>
2025-12-18 16:31:44 +0000

    Lösung gesichert

--- 18_Dezember/main.py
+++ 18_Dezember/main.py
@@ -5,11 +5,11 @@ hunger = 5
 # Aufgabe: Schreibe den Kopf der while-Schleife.
 # Solange hunger größer als 0 ist...
 # while ...:
-
+while hunger > 0:
     # (Erinnerung an Tag 11: f-String!)
     print(f"Habe noch Hunger ({hunger})... Mampf!")
     
     # WICHTIG: Ziehe hier 1 vom Hunger ab!
     # hunger = ...
-
+    hunger=hunger-1
 print("Pappsatt!")
