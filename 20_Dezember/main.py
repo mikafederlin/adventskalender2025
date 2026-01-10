0590ae63dedbb8cd5e69b79b41f2f6f686fb0f42
mikafederlin <mikafederlin@gmail.com>
2026-01-10 19:25:44 +0000

    Lösung gesichert

--- 20_Dezember/main.py
+++ 20_Dezember/main.py
@@ -5,9 +5,10 @@ def wiegen(paket1, paket2):
     # Aufgabe: Gib das Ergebnis 'gesamt' an das Hauptprogramm zurück.
     # Nutze den Befehl: return ...
 
-
+    return(gesamt)
 # Hier nutzen wir deine Funktion:
 # Wir speichern das, was zurück kommt (return), in der Variable 'gewicht'
 gewicht = wiegen(5, 10)
 
 print(f"Gesamtgewicht ist: {gewicht} kg")
+
